78b68b0d743bd51f4894495d9a57fca7142955b5
Hana-Houimli <hanahouimli003@gmail.com>
2025-11-06 20:49:12 +0100

    Exercice2

---
+++ Exercice2.ts
@@ -0,0 +1,14 @@
+//1
+let Name : string ='Hana';
+let Age : number = 22;
+let isAdmin : boolean = true;
+//2
+let scores : number[] = [1,2,3]; 
+console.log(scores);
+//3
+let student : [string, number]=["Hana",22];
+console.log(student);
+//4
+enum Role{User,Admin,SuperAdmin};
+let myrole : Role = Role.Admin;
+console.log(myrole);
